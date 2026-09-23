ed4f59c0aba83005f6fdf849a4f48bf8c3fde699
Aayush3115 <sapkotaaayush708@gmail.com>
2026-09-23 19:36:57 +0545

    Initial Commit

---
+++ Tiny Desktop Buddy/Tiny_Desktop_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_Desktop_BuddyApp.swift
+//  Tiny Desktop Buddy
+//
+//  Created by Aayush Sapkota on 23/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tiny_Desktop_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
